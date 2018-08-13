19c902becf73b66a68c8da1560c676ef8f4cd362
Barry Wardell <barry.wardell@gmail.com>
2018-08-13 13:19:57 +0100

    Make it so that ResummedSeriesData can be copy-pasted

--- ResummedSeries.m
+++ ResummedSeries.m
@@ -18,7 +18,7 @@ ResummedSeriesData[sf_, sd_]["SeriesData"] := sd;
 ResummedSeriesData[sf_, sd_] /; !MatchQ[sd, _SeriesData] := sf*sd;
 
 Format[rsd:ResummedSeriesData[__]] :=
- rsd["SingularFactor"] HoldForm[Evaluate[rsd["SeriesData"]]];
+ Interpretation[rsd["SingularFactor"] HoldForm[Evaluate[rsd["SeriesData"]]], rsd];
 
 End[];
 

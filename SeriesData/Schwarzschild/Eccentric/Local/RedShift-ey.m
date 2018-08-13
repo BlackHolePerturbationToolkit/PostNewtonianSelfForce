9109cb647f4643e533850e55d881a2509e33e058
Niels Warburton <nielsw@gmail.com>
2018-08-13 17:02:53 +0100

    Remove a temp change I made to the series in RedShift-ey.m

--- SeriesData/Schwarzschild/Eccentric/Local/RedShift-ey.m
+++ SeriesData/Schwarzschild/Eccentric/Local/RedShift-ey.m
@@ -1,7 +1,7 @@
 (* ::Package:: *)
 
 Module[{DeltaU},
-  DeltaU = SeriesData[y, 0, {-1, (-2) (-1 + e^2)^(-1) (-1 + 2 e^2), ResummedSeriesData[(1 - e^2)^(-2), SeriesData[e, 0, {-5, 0, 16, 0, Rational[-85, 8], 0, Rational[-9, 16], 0, Rational[-17, 128]Log[y], 0, Rational[-7, 256]}, 0, 12, 1]]}, 1, 4, 1];
+  DeltaU = SeriesData[y, 0, {-1, (-2) (-1 + e^2)^(-1) (-1 + 2 e^2), ResummedSeriesData[(1 - e^2)^(-2), SeriesData[e, 0, {-5, 0, 16, 0, Rational[-85, 8], 0, Rational[-9, 16], 0, Rational[-17, 128], 0, Rational[-7, 256]}, 0, 12, 1]]}, 1, 4, 1];
   <|
     "Name" -> "Schwarzschild Eccentric Orbit Red Shift Invariant",
     "Description" -> "Post-Newtonian expansion for the red shift invariant for a particle on an eccentric orbit about a Schwarzschild black hole to the linear order in the mass-ratio. Expansion parametrized in terms of e and y.",
